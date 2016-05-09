ea63494c705825238af3ca938f521cfe391c26b3
Andrew Russo and John Klein <john@>
2016-05-09 10:48:18 -0700

    Updated backend of the program for Many to Many testing.

--- src/test/java/CategoryTest.java
+++ src/test/java/CategoryTest.java
@@ -2,23 +2,12 @@ import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.Arrays;
+import java.util.List;
 
 public class CategoryTest {
 
-  @Before
-  public void setUp() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", null, null);
-  }
-
-  @After
-  public void tearDown() {
-    try(Connection con = DB.sql2o.open()) {
-      String deleteTasksQuery = "DELETE FROM tasks *;";
-      String deleteCategoriesQuery = "DELETE FROM categories *;";
-      con.createQuery(deleteTasksQuery).executeUpdate();
-      con.createQuery(deleteCategoriesQuery).executeUpdate();
-    }
-  }
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
 
   @Test
   public void Category_instantiatesCorrectly_true() {
@@ -68,14 +57,46 @@ public class CategoryTest {
   }
 
   @Test
-  public void getTasks_retrievesALlTasksFromDatabase_tasksList() {
+  public void addTask_addTaskToCategory_true() {
     Category myCategory = new Category("Household chores");
     myCategory.save();
-    Task firstTask = new Task("Mow the lawn", myCategory.getId());
-    firstTask.save();
-    Task secondTask = new Task("Do the dishes", myCategory.getId());
-    secondTask.save();
-    Task[] tasks = new Task[] { firstTask, secondTask };
-    assertTrue(myCategory.getTasks().containsAll(Arrays.asList(tasks)));
+    Task myTask = new Task("Mow the lawn");
+    myTask.save();
+    myCategory.addTask(myTask);
+    Task savedTask = myCategory.getTasks().get(0);
+    assertTrue(myTask.equals(savedTask));
   }
+
+  @Test
+  public void getTasks_returnsAllTasks_List() {
+    Category myCategory = new Category("Household chores");
+    myCategory.save();
+    Task myTask = new Task("Mow the lawn");
+    myTask.save();
+    myCategory.addTask(myTask);
+    List savedTasks = myCategory.getTasks();
+    assertEquals(1, savedTasks.size());
+  }
+
+  @Test
+  public void delete_deletesAllTaskAndCategoryAssociations() {
+    Category myCategory = new Category("Household chores");
+    myCategory.save();
+    Task myTask = new Task("Mow the lawn");
+    myTask.save();
+    myTask.addCategory(myCategory);
+    myTask.delete();
+    assertEquals(0, myCategory.getTasks().size());
+  }
+  // @Test
+  // public void getTasks_retrievesALlTasksFromDatabase_tasksList() {
+  //   Category myCategory = new Category("Household chores");
+  //   myCategory.save();
+  //   Task firstTask = new Task("Mow the lawn", myCategory.getId());
+  //   firstTask.save();
+  //   Task secondTask = new Task("Do the dishes", myCategory.getId());
+  //   secondTask.save();
+  //   Task[] tasks = new Task[] { firstTask, secondTask };
+  //   assertTrue(myCategory.getTasks().containsAll(Arrays.asList(tasks)));
+  // }
 }
